e10cba99ababf74be2b1e7df5a53365919e4308e
Breno Wruck Schneider <brenows@gmail.com>
2017-11-23 19:12:21 -0200

    Recipe Item now moved to the recipe item component. Header links now display the proper section (recipes or shopping list).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  display : string = 'recipes';
+
+  onRecipesClick() {
+    this.display = 'recipes';
+  }
+
+  onShoppingListClick() {
+    this.display = 'shopping-list'
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,19 @@
-import {Component} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector : 'app-header',
     templateUrl : './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    @Output() recipesClicked = new EventEmitter<{name: string, content: string}>();
+    @Output() shoppingListClicked = new EventEmitter<{name: string, content: string}>();
+    
+    onRecipesClick() {
+        this.recipesClicked.emit();
+    }
+
+    onShoppingListClick() {
+        this.shoppingListClicked.emit();
+    }
+
+}
