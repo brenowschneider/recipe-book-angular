2d0d6f49f6ab2b8f1157bd1b878e6794e2c27ffb
Breno Wruck Schneider <brenows@gmail.com>
2017-11-28 09:12:31 -0200

    Fixed the folder structure of shopping-list. Added shopping list service.

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+export class ShoppingListService {
+    
+    IngredientAdded = new EventEmitter<Ingredient>();
+    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 4),
+    ];
+
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+    
+    AddIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.IngredientAdded.emit(ingredient);
+    }
+}
