018f434c101140d8cd660b7bca75f7c708111b1c
Breno Wruck Schneider <brenows@gmail.com>
2018-02-23 07:25:16 -0300

    Fetch and Save data are now private functionalities (firebase access token)

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse} from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpParams} from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
     url = 'https://ng-recipe-book-2018.firebaseio.com/recipes.json';
-    constructor(private httpClient: HttpClient, private recipeService: RecipeService) {}
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
-       return this.httpClient.put(this.url, this.recipeService.getRecipes());
+        const params = new HttpParams().set('auth', this.authService.getToken());
+        return this.httpClient.put(this.url, this.recipeService.getRecipes(), {params : params});
     }
 
     getRecipes() {
-        this.httpClient.get<Recipe[]>(this.url)
+        const params = new HttpParams().set('auth', this.authService.getToken());
+        this.httpClient.get<Recipe[]>(this.url, {params : params})
             .subscribe(
                 (response: Recipe[]) => {
                     console.log(response);
