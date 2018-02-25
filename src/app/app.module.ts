65b87a44cfbf6538be1c592388b321bfe503e5c9
Breno Wruck Schneider <brenows@gmail.com>
2018-02-25 09:54:33 -0300

    Moves Services to Core Module and AuthGuard to Recipes Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { RecipeService } from './recipes/recipe.service';
-import { AppRoutingModule } from './app-routing.module';
-import { DataStorageService } from './shared/data-storage.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -26,7 +22,7 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
